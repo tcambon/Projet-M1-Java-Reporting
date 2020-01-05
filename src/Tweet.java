46ec85e1812028ea40168671abed20d0955df7b8
thomas <thomascambon38@gmail.com>
2020-01-05 22:17:15 +0100

    Revert "Revert "Creation of the project, data loading available""
    This reverts commit 2d54b70cafb80a65cc39168dbd2fe0b6601287cd.

---
+++ src/Tweet.java
@@ -0,0 +1,36 @@
+public class Tweet {
+    private long idTweet;
+    private String idUtilisateur;
+    private String datePubli;
+    private String contenu;
+    private String idUtilisateurRT;
+
+    public Tweet(long idTweet, String idUtilisateur, String datePubli, String contenu, String idUtilisateurRT) {
+        this.idTweet = idTweet;
+        this.idUtilisateur = idUtilisateur;
+        this.datePubli = datePubli;
+        this.contenu = contenu;
+        this.idUtilisateurRT = idUtilisateurRT;
+    }
+
+    @Override
+    public String toString() {
+        return "Tweet{" +
+                "idTweet=" + idTweet +
+                ", idUtilisateur='" + idUtilisateur + '\'' +
+                ", datePubli='" + datePubli + '\'' +
+                ", contenu='" + contenu + '\'' +
+                ", idUtilisateurRT='" + idUtilisateurRT + '\'' +
+                '}';
+    }
+
+    public boolean hasRetweet(){
+        return idUtilisateurRT != null;
+        }
+
+    }
+
+
+
+
+
