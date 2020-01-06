38033194943835ad0fba53d204a244372e0ac901
toms38 <49445991+toms38@users.noreply.github.com>
2020-01-06 12:04:59 +0100

    Add files via upload

--- src/BaseTweet.java
+++ src/BaseTweet.java
@@ -1,26 +1,39 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.sql.Timestamp;
+import java.time.temporal.TemporalAdjusters;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import static java.time.temporal.TemporalAdjusters.firstDayOfMonth;
 
 public class BaseTweet {
 
     private ArrayList<Tweet> tweets;
+    private int nbTweet = 0;
+    private Map<String, ArrayList<Tweet>> mapMois;
+    private Map<String, ArrayList<Tweet>> mapJour;
+    private Map<String, ArrayList<Tweet>> mapAnnee;
 
     public void initialise() {
         tweets = new ArrayList<>();
+        mapMois = new TreeMap<>();
+        mapJour = new TreeMap<>();
+        mapAnnee = new TreeMap<>();
+
+
     }
 
 
     public void ouvrirCSV() {
         try {
-            FileReader fr = new FileReader("climat.txt");
+            FileReader fr = new FileReader("data/climat.txt");
             BufferedReader br = new BufferedReader(fr);
             String line;
             Tweet montweet;
-
             int lineNb = 1;
+            int nbErr = 0;
+
+
             long initTime = System.currentTimeMillis();
             while ((line = br.readLine()) != null) {
                 String[] arrLine = line.split("\t");
@@ -31,6 +44,7 @@ public class BaseTweet {
                         idTweet = Long.parseLong(arrLine[0]);
                     }
                 } catch (Exception ex) {
+                    nbErr++;
                     System.err.println("Invalid idTweet for line " + lineNb);
                 }
 
@@ -39,10 +53,21 @@ public class BaseTweet {
                     idUtilisateur = arrLine[1];
                 }
 
-                String datePubli = null;
+                Timestamp datePubli = null;
+                Integer annee = null;
+                Integer mois = null;
+                Integer jour = null;
                 if (arrLine.length > 2) {
-                    datePubli = arrLine[2];
+                    datePubli = Timestamp.valueOf(arrLine[2]);
+                    // recuperation du jour, mois et annee
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTimeInMillis(datePubli.getTime());
+                    annee = cal.get(Calendar.YEAR);
+                    // Calendar.MONTH commence a 0, on ajoute 1 pour avoir le bon num de mois
+                    mois = cal.get(Calendar.MONTH) + 1;
+                    jour = cal.get(Calendar.DAY_OF_MONTH);
                 }
+
 
                 String contenu = null;
                 if (arrLine.length > 3) {
@@ -56,12 +81,55 @@ public class BaseTweet {
 
                 montweet = new Tweet(idTweet, idUtilisateur, datePubli, contenu, idUtilisateurRT);
                 tweets.add(montweet);
+                String moisStr = null;
+                if (mois!=null){
+                    moisStr=String.valueOf(mois);
+                    if(mois<10){
+                        moisStr="0"+moisStr;
+                    }
+                    String MoisAnnee = moisStr + String.valueOf(annee) ;
+                    // on initialise si l'array list n'est pas encore creee
+                    ArrayList<Tweet> listMois = mapMois.get(MoisAnnee);
+                    if (listMois == null){
+                        listMois = new ArrayList<>();
+                    }
+                    listMois.add(montweet);
+                    mapMois.put(MoisAnnee, listMois);
+                }
+
+                if (jour!=null){
+                    String jourMoisAnnee = String.valueOf(jour)+moisStr+ String.valueOf(annee);
+                    // on initialise si l'array list n'est pas encore creee
+                    ArrayList<Tweet> listJour = mapJour.get(jourMoisAnnee);
+                    if (listJour == null){
+                        listJour = new ArrayList<>();
+                    }
+                    listJour.add(montweet);
+                    mapJour.put(jourMoisAnnee, listJour);
+                }
+
+                if (annee!=null){
+                    // on initialise si l'array list n'est pas encore creee
+                    ArrayList<Tweet> listAnnee = mapJour.get(String.valueOf(annee));
+                    if (listAnnee == null){
+                        listAnnee = new ArrayList<>();
+                    }
+                    listAnnee.add(montweet);
+                    mapAnnee.put(String.valueOf(annee), listAnnee);
+                }
+
+
+
                 lineNb++;
             }
-            long elapseTime=System.currentTimeMillis() - initTime;
+            nbTweet = lineNb - nbErr;
+
+            long elapseTime = System.currentTimeMillis() - initTime;
             System.out.println(lineNb + " lignes chargées en " + elapseTime + " ms");
+            System.out.println(nbTweet + " Tweet chargés");
             br.close();
             fr.close();
+
 
 
         } catch (Exception exception) {
@@ -74,7 +142,7 @@ public class BaseTweet {
     public String afficher() {
         StringBuilder sb = new StringBuilder();
         //on affiche seulement les 15 premiers
-        for (int ii = 0; ii < 16; ii++) {
+        for (int ii = 0; ii < 15; ii++) {
             sb.append(ii + " " + tweets.get(ii).toString() + "\n");
         }
         return sb.toString();
